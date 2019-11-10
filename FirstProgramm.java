7b8b92c08617cc0018ea46403076ca73ea09de13
rana-hamraeva <56229982+rana-hamraeva@users.noreply.github.com>
2019-11-10 14:17:23 -0600

    first push

---
+++ FirstProgramm.java
@@ -0,0 +1,10 @@
+public class FirstProgramm {
+	
+	public static void main(String[] args){
+
+	int age;
+	age = 25;
+	System.out.println(age);
+	System.out.println("Hey guys! I am " + age + " years old!");
+	}
+}
